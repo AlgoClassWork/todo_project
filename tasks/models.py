a9ea2fbde9247b0793d0d0449ee64a3189709226
AlgoClassWork <kyxec.play@gmail.com>
2025-07-10 12:54:58 +0600

    Add files via upload

---
+++ tasks/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# Create your models here.
+class Task(models.Model):
+
+    title = models.CharField('Название задачи', max_length=200)
+    description = models.TextField('Описание', blank=True, null=True)
+    completed = models.BooleanField('Выполнено', default=False)
+    created_at = models.DateTimeField('Дата создания', auto_now_add=True)
